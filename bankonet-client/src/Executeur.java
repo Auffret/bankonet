cd58a2388097dfa570d5a7d1e156d6a18524b2db
Auffret <davy.auffret@gmail.com>
2015-10-16 10:22:57 +0200

    add repertory
    client
    conseiller
    lib

---
+++ bankonet-client/src/Executeur.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Executeur {
+	public static void main(String[] args) {
+		
+		Console exe=new Console();
+		
+	}
+
+}
---
+++ bankonet-conseiller/src/Executeur.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Executeur {
+
+	public static void main(String[] args) {
+		
+		Console exe=new Console();
+		
+		
+		
+	}
+
+}

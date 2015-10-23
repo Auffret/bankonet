b4506dbace6dec6b08c662daabdbe538fbd82eb0
Auffret <davy.auffret@gmail.com>
2015-10-23 16:43:14 +0200

    test

---
+++ bankonet-data/src/bin/main/sql/init-db.sql
@@ -0,0 +1,5 @@
+INSERT INTO client VALUES("nom1", "prenom1", "log1", "pass1");
+INSERT INTO client VALUES("nom2", "prenom2", "log2", "pass2");
+INSERT INTO client VALUES("nom3", "prenom3", "log3", "pass3");
+INSERT INTO client VALUES("nom4", "prenom4", "log4", "pass4");
+INSERT INTO client VALUES("nom5", "prenom5", "log5", "pass5");
